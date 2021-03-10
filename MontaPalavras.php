f4b36cf4e59999c06c0f0a60d9dd724708d22ee9
Danilo D. Vaz <danilodvaz@gmail.com>
2021-03-10 19:17:01 -0300

    Inicio da construção das palavras.

--- MontaPalavras.php
+++ MontaPalavras.php
@@ -14,9 +14,9 @@ class MontaPalavras
             $letrasValidas = $this->retornaLetras($entrada);
             $letrasNaoUsadas = $this->retornaCaracteresEspeciais($entrada);
 
-            
-            var_dump('$letrasValidas', $letrasValidas);
-            var_dump('$letrasNaoUsadas', $letrasNaoUsadas);
+            $this->constroiPalavras($letrasValidas);
+            // var_dump('$letrasValidas', $letrasValidas);
+            // var_dump('$letrasNaoUsadas', $letrasNaoUsadas);
 
 
         } while ($entrada != 'exit');
@@ -30,5 +30,41 @@ class MontaPalavras
     private function retornaCaracteresEspeciais($string)
     {
         return preg_replace("/[a-z ]+/i", "", $string); 
+    }
+
+    private function retornaBancoPalavras()
+    {
+        $bancoPalavrasJson = file_get_contents('./BancoPalavras.json');
+        $bancoPalavras = json_decode($bancoPalavrasJson, true);
+
+        return $bancoPalavras['palavras'];
+    }
+
+    private function constroiPalavras($letrasValidas)
+    {
+        $palavras = $this->retornaBancoPalavras();
+        $listaLetras = str_split($letrasValidas);
+
+        foreach ($palavras as $palavra) {
+            foreach ($listaLetras as $letra) {
+                if (!empty($palavra)) {
+                    $posicaoLetra = strpos($palavra, $letra);
+
+                    if ($posicaoLetra === false) {
+                        // Letra não utilizada para formar a palavra
+                    } else {
+                        $palavra = substr_replace($palavra, '', $posicaoLetra, 1);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if (empty($palavra)) {
+                // adiciona a palavra na lista de montadas
+                // adiciona as letras não utilizadas na lista de letras não utilizadas.
+            }
+        }
+
     }
 }
